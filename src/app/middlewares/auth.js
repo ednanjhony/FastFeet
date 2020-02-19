dea5e706e3adf2165777e4982a807e0ad7dad826
Ednan Jhony <ednanjhony@yahoo.com.br>
2020-02-19 06:40:45 -0300

    Modified

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -17,6 +17,7 @@ export default async (req, res, next) => {
 
     req.userId = decoded.id;
     req.recipientId = decoded.id;
+    req.deliverymanId = decoded.id;
 
     return next();
   } catch (err) {
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
+import DeliverymanController from './app/controllers/Deliveryman';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -19,5 +24,10 @@ routes.put('/users', UserController.update);
 // CRUD RECIPIENT
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/deliveryman', DeliverymanController.store);
+routes.put('/deliveryman', DeliverymanController.update);
 
 export default routes;
